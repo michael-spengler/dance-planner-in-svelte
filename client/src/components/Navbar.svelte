288a8285187aa48454b80f0dbbe78af61d8b1e31
Michael <michael.spengler@sap.com>
2021-07-01 14:45:26 +0200

    add very basic skeleton

---
+++ client/src/components/Navbar.svelte
@@ -0,0 +1,75 @@
+<script>
+// let navbarData = {
+//       logoURL: '../assets/logo.png',
+//       appTitle: (domain === 'localhost:4200') ? 'dance-planner.org' : domain,
+//       menuEntries: [{
+//         isActive: true,
+//         text: 'Find Events',
+//         href: 'find',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'Create Events',
+//         href: 'create',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'The App',
+//         href: 'app',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'Dance Partners',
+//         href: 'findDancePartners',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'Tutorials',
+//         href: 'findTutorials',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'Outfits',
+//         href: 'findOutfits',
+//       },
+
+//                     {
+//         isActive: false,
+//         text: 'Bed & Breakfast',
+//         href: 'bedAndBreakfast',
+//       },
+//                     {
+//         isActive: false,
+//         text: 'Dance Schools',
+//         href: 'findSchools',
+//       },
+
+//                     {
+//         isActive: false,
+//         text: 'Contact',
+//         href: 'contact',
+//       }],
+//     }
+</script>
+
+hallo
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="topnav" id="myTopnav">
+  <span (click)="clickTitle()">
+    <img class="logo" src="{navBarData.logoURL}" alt="">
+    <a class="ellipsis navbarTitle" style="font-size: 20.6px; text-align: center">
+      {navBarData.appTitle}</a>
+  </span>
+  <span *ngFor="let item of navBarData.menuEntries">
+    <a id="{item.href}" style="font-size: 20.6px;" class="hide" [ngClass]="{active: item.isActive}"
+      (click)="clickEntry(item.href)">{{item.text}}</a>
+  </span>
+  <a class="icon" (click)="myFunction()" style="font-size:24px">
+    <i id="burgerMenu" class="fa fa-bars" style="color: rgb(213, 54, 84);"></i>
+  </a>
+</div> -->
+
+
+<style>
+</style>
